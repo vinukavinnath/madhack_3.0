8a8b51b5484f4e19573a3f1f5268fc91da34dc22
Vinuka Vinnath <vinnathwlv@gmail.com>
2024-03-11 00:43:30 +0530

    feat: Assets defined, Constants added

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//Primary Colors
+const kBlackColor=Color(0xff000000);
+const kWhiteColor=Color(0xffffffff);
+
+//Color Palette
+const kDeepBlueColor = Color(0xFF16056B);
+const kAmberColor = Color(0xFFFD9519);
+const kOceanBlueColor = Color(0xFF5696FA);
+const kLightBlueColor = Color(0xFFB4D7FE);
+const kAshWhiteColor = Color(0xFFF4F7F9);
+const kDarkGreyColor = Color(0xFF4B4646);
+const kLowOpacityLightBlueColor = Color.fromARGB(102, 180, 215, 254);

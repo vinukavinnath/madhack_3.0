0ffac03dd7526498f3c1fdc0a66f0e659529e70b
unknown <yasaruwanbasuru637@gmail.com>
2024-03-17 10:22:25 +0530

    Update the jobcarduser page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:async_and_await/controller/root_page.dart';
 import 'package:async_and_await/view/pages/authentication/splash_screen.dart';
 import 'package:async_and_await/view/pages/user/dashboard.dart';
 import 'package:async_and_await/view/pages/user/dashboard/aboutme.dart';
+import 'package:async_and_await/view/pages/user/dashboard/jobshowing.dart';
 import 'package:async_and_await/view/widgets/hidden_drawer_employer.dart';
 import 'package:async_and_await/view/widgets/hidden_drawer_user.dart';
 
@@ -41,8 +42,8 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const SafeArea(
-        child: HiddenDrawer(),
+      home:  SafeArea(
+        child: SplashScreen(),
       ),
       // home: const SafeArea(
       //   child: VerifyAccount(),
